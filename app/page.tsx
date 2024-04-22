613b6a2f09fbfcdd767c3f8bc2502da0e79d9853
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-04-22 13:37:22 +0100

    project set-up

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant='destructive'>Delete</Button>
+    </main>
+  );
+}
