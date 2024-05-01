bb92c55ec058b3a70b433a71b0b6d82743d4214a
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-05-01 15:55:41 +0100

    created serverless function that creates new user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import { CreateUserParams } from '@/types';
+import { handleError } from '@/lib/utils';
+import { connectToDatabase } from '@/lib/mongodb/database/index';
+import User from '@/lib/mongodb/database/models/user.model';
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase();
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
