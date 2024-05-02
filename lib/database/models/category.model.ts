7e066d8f25e152d5f307464daea9cd206c469d5d
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-05-02 03:27:10 +0100

    fixed deployment bug

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,4 +1,4 @@
-import { Schema. Document, models, model} from 'mongoose';
+import { Schema, Document, models, model} from 'mongoose';
 
 export interface ICategory extends Document {
   _id: string;
